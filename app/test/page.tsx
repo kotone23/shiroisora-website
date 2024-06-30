d64a7a772fb656ca8804980314f3eb70d4907b8d
kento-yoshida <kento-yoshida@kke.co.jp>
2024-06-30 22:58:02 +0000

    chore: Make a test top page to illustrate logo position

---
+++ app/test/page.tsx
@@ -0,0 +1,42 @@
+import NavBar from "@/app/components/NavBar";
+import SpNav from "@/app/components/SpNav";
+import type { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+	title: "WHITE SKY",
+	description: "WHITE SKY",
+};
+
+export default function Home() {
+	return (
+		<>
+			<div className="fixed h-screen w-screen top-0 -z-50">
+				<Image
+					alt="Mountains"
+					src="/images/background.jpg"
+					quality={100}
+					fill
+					sizes="100vw"
+					style={{
+						objectFit: "cover",
+					}}
+					className="animate-slide-in-bck-center"
+				/>
+			</div>
+			<SpNav />
+			<NavBar />
+			<div className="flex justify-center items-center min-h-screen">
+				<div className="animate-slide-in-bck-center">
+					<Image
+						src="/images/logo_black.png"
+						alt="logo"
+						width={128}
+						height={128}
+						priority
+					/>
+				</div>
+			</div>
+		</>
+	);
+}
