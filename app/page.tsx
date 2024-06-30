6c8232f7827b2d15c987914842aa5c06bd815bc1
kento-yoshida <kento-yoshida@kke.co.jp>
2024-06-30 22:32:42 +0000

    chore: Delete logo position style

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 			<SpNav />
 			<NavBar />
 			<div className="flex justify-center items-center min-h-screen">
-				<div className="relative -top-0 animate-slide-in-bck-center">
+				<div className="animate-slide-in-bck-center">
 					<Image
 						src="/images/logo_black.png"
 						alt="logo"
