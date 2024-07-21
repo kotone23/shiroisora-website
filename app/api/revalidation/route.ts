091ae4cac8348b806a55ed6b157536f7a7612c4e
kento-yoshida <kento-yoshida@kke.co.jp>
2024-07-21 21:22:24 +0000

    chore: Update revalidation route to handle metadata tags for revalidation

--- app/api/revalidation/route.ts
+++ app/api/revalidation/route.ts
@@ -2,13 +2,18 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
+type ContentfulEntryType = "live" | "media" | "release" | "liveMusicClub" | "song";
+
 type ContentfulRequestBody = {
+	metadata: {
+		tags?: ContentfulEntryType[];
+	}
 	sys: {
 		type: "Entry" | "Asset" | "ContentType";
 		id: string;
 		contentType?: {
 			sys: {
-				id: "live" | "media" | "release" | "liveMusicClub" | "song";
+				id: ContentfulEntryType;
 			};
 		};
 	};
@@ -22,8 +27,16 @@ export async function POST(request: NextRequest) {
 		return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
 	}
 
-	// if not entry is updated, no revalidation is needed
+	// if metadata.tags includes any tag, revalidate the tags
 	const body: ContentfulRequestBody = await request.json();
+	if (body.metadata.tags?.length) {
+		for (const tag of body.metadata.tags) {
+			revalidateTag(tag);
+		}	
+		return NextResponse.json({ revalidated: true, updatedType: body.metadata.tags });
+	}
+
+	// if not entry is updated, no revalidation is needed
 	if (body.sys.type !== "Entry") {
 		return NextResponse.json({ revalidated: false, updatedType: body.sys.type });
 	} 
