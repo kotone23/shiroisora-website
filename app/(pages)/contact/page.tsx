aed5a67784c16c926abd398cef52203ca080580a
kento-yoshida <kento-yoshida@kke.co.jp>
2024-08-12 22:41:05 +0000

    chore: Update Google Form link for ticket reservations

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -15,10 +15,19 @@ export default function ContactPage() {
 			<h1 className="flex justify-center text-4xl font-bold mt-8 mb-8">
 				Contact
 			</h1>
-			<p className="flex justify-center mt-10 mb-10 w-10/12 sm:w-8/12 mx-auto">
+			<p className="justify-center mt-10 mb-10 w-10/12 sm:w-8/12 mx-auto">
 				ライブチケットのご予約や出演依頼はこちらのフォームからお願いいたします。
 				<br />
-				チケットのご予約の場合はライブの日程と枚数を必ずご記入ください（Googleフォームからもご予約いただけます）。
+				チケットのご予約の場合はライブの日程と枚数を必ずご記入ください（
+				<a
+					href="https://forms.gle/AFFUcEWdBtPC99g2A"
+					target="_blank"
+					rel="noreferrer"
+					className="text-blue-800 underline"
+				>
+					Googleフォーム
+				</a>
+				からもご予約いただけます）。
 			</p>
 			<Inquiry />
 		</main>
