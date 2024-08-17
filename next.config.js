c4738341273290283699365b3c7a9505916ca674
kento-yoshida <kento-yoshida@kke.co.jp>
2024-08-17 06:39:14 +0000

    chore: Add redirects for HTML pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/index.html",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/live.html",
+				destination: "/live",
+				permanent: true,
+			},
+      {
+        source: "/discography.html",
+        destination: "/discography",
+        permanent: true,
+      },
+      {
+        source: "/media.html",
+        destination: "/media",
+        permanent: true,
+      },
+      {
+        source: "/contact/postmail.html",
+        destination: "/contact",
+        permanent: true,
+      },
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
