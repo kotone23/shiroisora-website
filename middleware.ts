dd310d32cc806fc11a4ecdd10412c4596f6a3b10
kento-yoshida <kento-yoshida@kke.co.jp>
2024-08-25 21:39:57 +0000

    Basic認証を外す

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,38 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from "next/server";
 
+// Basic Authentication middleware
+// No pages is protected by basic authentication now
 export const config = {
-  matcher: ['/:path*']
-}
+	matcher: [], // "/:path*" is a catch-all route
+};
 
 export function middleware(req: NextRequest) {
-  const isLocalDevelopment = process.env.NODE_ENV === 'development';
+	const isLocalDevelopment = process.env.NODE_ENV === "development";
 
-  if (isLocalDevelopment || !process.env.BASIC_ID || !process.env.BASIC_PWD) {
-    return NextResponse.next();
-  }
+	if (isLocalDevelopment || !process.env.BASIC_ID || !process.env.BASIC_PWD) {
+		return NextResponse.next();
+	}
 
-  const basicAuth = req.headers.get('authorization');
-  if (!basicAuth) {
-    return new Response('Authentication required', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"',
-      },
-    });
-  }
+	const basicAuth = req.headers.get("authorization");
+	if (!basicAuth) {
+		return new Response("Authentication required", {
+			status: 401,
+			headers: {
+				"WWW-Authenticate": 'Basic realm="Secure Area"',
+			},
+		});
+	}
 
-  try {
-    const authValue = basicAuth.split(' ')[1];
-    const [user, pwd] = atob(authValue).split(':');
+	try {
+		const authValue = basicAuth.split(" ")[1];
+		const [user, pwd] = atob(authValue).split(":");
 
-    if (user === process.env.BASIC_ID && pwd === process.env.BASIC_PWD) {
-      return NextResponse.next();
-    }
-  } catch (e) {
-    return new Response('Invalid Authentication', { status: 400 });
-  }
+		if (user === process.env.BASIC_ID && pwd === process.env.BASIC_PWD) {
+			return NextResponse.next();
+		}
+	} catch (e) {
+		return new Response("Invalid Authentication", { status: 400 });
+	}
 
-  return new Response('Unauthorized', { status: 401 });
+	return new Response("Unauthorized", { status: 401 });
 }
