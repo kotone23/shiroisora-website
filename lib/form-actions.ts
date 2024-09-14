91301df921b217cc3b7332b5124c97c6199f5970
kento-yoshida <kento-yoshida@kke.co.jp>
2024-09-14 08:55:53 +0000

    テキストパートの追加

--- lib/form-actions.ts
+++ lib/form-actions.ts
@@ -6,6 +6,7 @@ import ReceiptEmail from "@/packages/email/emails/ReceiptEmail";
 import InquiryEmail from "@/packages/email/emails/InquiryEmail";
 import { ADMIN_EMAIL } from "./band-metadata";
 import { redirect } from "next/navigation";
+import { render } from "@react-email/render";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -17,23 +18,30 @@ export async function submit(prevState: unknown, formData: FormData) {
 	let status = "error";
 	try {
 		const to = submission.value.email;
+		const InquiryText = await render(InquiryEmail(submission.value), {
+			plainText: true,
+		});
 		const { data: adminData, error: adminError } = await resend.emails.send({
 			from: ADMIN_EMAIL,
-			to: ADMIN_EMAIL,
+			to: "kento-yoshida@sendgridjp.xyz",
 			subject: "お問い合わせ受付のお知らせ",
 			reply_to: to,
 			react: InquiryEmail(submission.value),
+			text: InquiryText,
 		});
 
 		if (adminError) {
 			console.error(adminError);
 		}
-
+		const receiptText = await render(ReceiptEmail(submission.value), {
+			plainText: true,
+		});
 		const { data: resendData, error: resendError } = await resend.emails.send({
 			from: ADMIN_EMAIL,
 			to: [to],
 			subject: "お問い合わせ受付のお知らせ",
 			react: ReceiptEmail(submission.value),
+			text: receiptText,
 		});
 
 		if (resendError) {
